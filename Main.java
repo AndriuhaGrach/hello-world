b6f1a34c1f4fdb1039699c722d65b91b4a1d4ab9
AndriuhaGrach <81981366+AndriuhaGrach@users.noreply.github.com>
2021-12-17 16:02:31 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,32 @@
 import java.util.Scanner;
+import library.Library;
 
-public class Main extends Recursion {
+public class Main extends Library {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print(factorialMessage + colon);
-        int i = scanner.nextInt();
-        factorialOutput(i);
+        for (int number = -1; number != 0;) {
+            for (String s : menu) System.out.print("\n" + s);
+            number = scanner.nextInt();
+            int i;
+            boolean test = false;
+            switch (number) {
+                case 1:
+                    System.out.print(factorialMessage);
+                    while (!test) {
+                        i = scanner.nextInt();
+                        test = factorialOutput(i);
+                    }
+                    break;
+                case 2:
+                    System.out.print(fibonacciMessage);
+                    while (!test) {
+                        i = scanner.nextInt();
+                        test = fibonacciOutput(i);
+                    }
+                    break;
+            }
+        }
+        System.out.print("Exit.");
     }
 }
--- Recursion.java
+++ Recursion.java
@@ -1,4 +1,6 @@
-class Recursion implements TextMessages {
+package library;
+
+interface Recursion extends TextMessages {
 
     //Finding factorial from 0 to 20
     private static long factorial(int x) {
@@ -6,8 +8,32 @@ class Recursion implements TextMessages {
         else return x * factorial(x - 1);
     }
 
-    protected static void factorialOutput(int x) {
-        if (x > 20 || x < 0) System.out.println(factorialMessage + period);
-        else System.out.println(factorial(x));
+    static boolean factorialOutput(int x) {
+        if (x > 20 || x < 0) {
+            System.out.print(factorialMessage);
+            return false;
+        }
+        else {
+            System.out.print(answer + factorial(x));
+            return true;
+        }
+    }
+
+    //Finding fibonacci from 0 to 36
+    private static int fibonacci(int n){
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    static boolean fibonacciOutput(int n) {
+        if (n < 0 || n > 36) {
+            System.out.print(fibonacciMessage);
+            return false;
+        }
+        else {
+            System.out.print(answer + fibonacci(n));
+            return true;
+        }
     }
 }
